17cd12057650ffb5468e7b3b72973f34db7302f5
sharils <sharils@users.noreply.github.com>
2015-11-30 11:04:33 +0800

    Add sharils curl

---
+++ spec/CurlSpec.php
@@ -0,0 +1,269 @@
+<?php
+
+namespace spec\Sharils;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Sharils\Curl;
+
+class CurlSpec extends ObjectBehavior
+{
+    const CONTENT = 'Lorem ipsum dolor sit amet.';
+
+    const NAME = 'Lorem';
+
+    public function getMatchers()
+    {
+        static $prefix = 'should';
+
+        $methods = get_class_methods($this);
+
+        $methods = array_filter($methods, function ($method) use ($prefix) {
+            return strpos($method, $prefix) === 0;
+        });
+
+        $matchers = [];
+        $prefixLength = strlen($prefix);
+        foreach ($methods as $method) {
+            $key = substr($method, $prefixLength);
+            $key = lcfirst($key);
+            $matchers[$key] = [$this, $method];
+        }
+
+        return $matchers;
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType('Sharils\Curl');
+    }
+
+    public function it_executes_parallel()
+    {
+        $begin = microtime(true);
+        $this->curl(array_pad(
+            [],
+            5,
+            $this->usleep(200000)
+        ))->shouldFinishBefore($begin + 200000 * 5 / 1000000);
+    }
+
+    public function it_is_array_in_array_out()
+    {
+        $this->curl(array_pad(
+            [],
+            2,
+            $this->blank()
+        ))->shouldBeResourcesOf('curl');
+    }
+
+    public function it_sets_curl_resource_content()
+    {
+        $this->curl(array_pad(
+            [],
+            2,
+            $this->printR(self::CONTENT)
+        ))->shouldSetCurlResourceContent(self::CONTENT);
+    }
+
+    public function it_throws_error()
+    {
+        $this->shouldThrow('RuntimeException')->duringCurl([
+            $this->blank(),
+            [],
+        ]);
+    }
+
+    public function it_reuses_connection()
+    {
+        $this->curl([$this->blank()]);
+
+        $this->curl([$this->verbose($log)])->
+            shouldReuseConnection($log);
+    }
+
+    public function it_parses_response()
+    {
+        list($curl) = $this->curl([$this->header(self::CONTENT)]);
+
+        $this->parseResponse($curl)->shouldBeParsed([
+            self::CONTENT,
+            (object) [
+                'server' => 'Apache/2.4.10 (Debian) PHP/7.0.0',
+                'xPoweredBy' => 'PHP/7.0.0',
+                'contentLength' => (string) strlen(self::CONTENT),
+                'contentType' => 'text/html; charset=UTF-8',
+            ]
+        ]);
+    }
+
+    public function it_converts_empty_body_to_null()
+    {
+        list($curl) = $this->curl([$this->header('')]);
+
+        $this->parseResponse($curl)->shouldBeParsed([
+            null,
+            (object) [
+                'server' => 'Apache/2.4.10 (Debian) PHP/7.0.0',
+                'xPoweredBy' => 'PHP/7.0.0',
+                'contentLength' => '0',
+                'contentType' => 'text/html; charset=UTF-8',
+            ]
+        ]);
+    }
+
+    public function it_honours_defaults()
+    {
+        $this->setDefaults($this->printR(self::CONTENT));
+
+        $this->curl(array_pad(
+            [],
+            2,
+            []
+        ))->shouldSetCurlResourceContent(self::CONTENT);
+    }
+
+    public function it_tries_pipelining()
+    {
+        $this->setOptions([CURLMOPT_PIPELINING => 2]);
+
+        $this->curl(array_pad([], 2, $this->verbose($log)))->
+            shouldTryPipeling($log);
+    }
+
+    public function it_honours_share()
+    {
+        $sh = $this->share([
+            CURL_LOCK_DATA_COOKIE => CURLSHOPT_SHARE
+        ]);
+
+        $this->curl([
+            $this->cookie($sh, self::NAME, self::CONTENT)
+        ]);
+
+        $this->curl([
+            $this->cookie($sh, self::NAME, self::CONTENT)
+        ])->shouldSendCookie(self::NAME, self::CONTENT);
+    }
+
+    public function shouldBeParsed($subject, $parsed)
+    {
+        unset($subject[1]->date);
+
+        return $subject == $parsed;
+    }
+
+    public function shouldBeResourcesOf(array $subjects, $type)
+    {
+        $result = true;
+        foreach ($subjects as $subject) {
+            $result = $result &&
+                is_resource($subject) &&
+                get_resource_type($subject) === $type;
+        }
+
+        return $result;
+    }
+
+    public function shouldFinishBefore($curls, $end)
+    {
+        return microtime(true) < $end;
+    }
+
+    public function shouldReuseConnection($subject, $log)
+    {
+        rewind($log);
+        $log = stream_get_contents($log);
+
+        return strpos($log, '* Re-using existing connection!') !== false;
+    }
+
+    public function shouldSendCookie($subject, $name, $value)
+    {
+        $info = curl_getinfo($subject[0], CURLINFO_HEADER_OUT);
+
+        $value = urlencode($value);
+
+        return strpos($info, "Cookie: $name=$value");
+    }
+
+    public function shouldSetCurlResourceContent(array $subjects, $content)
+    {
+        $set = true;
+        foreach ($subjects as $subject) {
+            $set = $set && curl_multi_getcontent($subject) === $content;
+        }
+
+        return $set;
+    }
+
+    public function shouldTryPipeling($subject, $log)
+    {
+        rewind($log);
+        $log = stream_get_contents($log);
+
+        return strpos($log, '* Server doesn\'t support pipelining') !== false;
+    }
+
+    private function testServer($query = null)
+    {
+        if ($query !== null) {
+            $param_arr = func_get_args();
+            $callback = array_shift($param_arr);
+
+            $query = '?' . http_build_query([
+                $callback => $param_arr
+            ]);
+        }
+
+        return 'http://' . TEST_SERVER . "/$query";
+    }
+
+    private function blank()
+    {
+        return [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_URL => $this->testServer()
+        ];
+    }
+
+    private function cookie($sh, $name, $value)
+    {
+        return [
+            CURLINFO_HEADER_OUT => true,
+            CURLOPT_SHARE => $sh,
+            CURLOPT_URL => $this->testServer('setcookie', $name, $value)
+        ];
+    }
+
+    private function header($content)
+    {
+        return $this->printR($content) + [
+            CURLOPT_HEADER => true
+        ];
+    }
+
+    private function printR($content)
+    {
+        return [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_URL => $this->testServer('print_r', $content)
+        ];
+    }
+
+    private function usleep($microSeconds)
+    {
+        return [
+            CURLOPT_URL => $this->testServer('usleep', $microSeconds)
+        ];
+    }
+
+    private function verbose(&$log)
+    {
+        return [
+            CURLOPT_VERBOSE => true,
+            CURLOPT_STDERR => $log = tmpfile(),
+            CURLOPT_URL => $this->testServer()
+        ];
+    }
+}
---
+++ src/Curl.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace Sharils;
+
+use InvalidArgumentException;
+use RuntimeException;
+use stdClass;
+
+class Curl
+{
+    const EOL = "\r\n";
+
+    const HEADER_BDOY = "\r\n\r\n";
+
+    private $defaults = [];
+
+    private $mh = null;
+
+    public function __construct()
+    {
+        $this->mh = curl_multi_init();
+    }
+
+    public function __destruct()
+    {
+        if (is_resource($this->mh) && get_resource_type($this->mh) === 'curl') {
+            curl_multi_close($this->mh);
+        }
+    }
+
+    public function curl(array $optionsList, array $options = [])
+    {
+        return call_user_func_array(function (
+            array $options,
+            array $otherOptions = null
+        ) use ($options) {
+            $curls = array_map([$this, 'opt2Curl'], func_get_args());
+
+            $errors = $this->exec($curls);
+            if (!empty($errors)) {
+                throw new RuntimeException($this->err2Msg($errors));
+            }
+
+            return $curls;
+        }, $optionsList);
+    }
+
+    public function parseResponse($curl)
+    {
+        $response = curl_multi_getcontent($curl);
+        assert('is_string($response)');
+
+        $size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+
+        $body = substr($response, $size) ?: null;
+
+        $header = substr($response, 0, $size);
+        $fields = explode(self::EOL, $header);
+        $fields = array_filter($fields, [$this, 'isField']);
+        $header = array_reduce($fields, [$this, 'setField']);
+
+        return [
+            $body,
+            (object) $header
+        ];
+    }
+
+    public function setDefaults(array $defaults)
+    {
+        $this->defaults = $defaults;
+    }
+
+    public function setOptions(array $options)
+    {
+        foreach ($options as $option => $value) {
+            $success = curl_multi_setopt($this->mh, $option, $value);
+            assert('$success === true');
+        }
+    }
+
+    public function share(array $options)
+    {
+        $sh = curl_share_init();
+
+        foreach ($options as $value => $key) {
+            $success = curl_share_setopt($sh, $key, $value);
+            assert('$success');
+        }
+
+        return $sh;
+    }
+
+    private function err2Msg(array $errors)
+    {
+        ksort($errors);
+        $message = [];
+        foreach ($errors as $errorIdx => $error) {
+            $message[] = $errorIdx . ': ' . curl_strerror($error);
+        }
+        $message = implode(PHP_EOL, $message);
+
+        return $message;
+    }
+
+    private function exec(array $curls)
+    {
+        $errors = [];
+        $re = null;
+
+        foreach ($curls as $curl) {
+            $error = curl_multi_add_handle($this->mh, $curl);
+            assert('$error === CURLM_OK');
+        }
+
+        do {
+            do {
+                $mrc = curl_multi_exec($this->mh, $active);
+            } while ($mrc == CURLM_CALL_MULTI_PERFORM);
+
+            if ($mrc !== CURLM_OK) {
+                $re = new RuntimeException(curl_multi_strerror($mrc), $mrc);
+                break;
+            }
+
+            do {
+                $info = curl_multi_info_read($this->mh);
+                if (isset($info['result']) && $info['result'] !== CURLE_OK) {
+                    $errors[array_search($info['handle'], $curls)] =
+                        $info['result'];
+                }
+            } while ($info);
+        } while ($active && (~curl_multi_select($this->mh) || !usleep(100)));
+
+        foreach ($curls as $curl) {
+            $error = curl_multi_remove_handle($this->mh, $curl);
+            assert('$error === CURLM_OK');
+        }
+
+        if ($re) {
+            throw $re;
+        }
+
+        return $errors;
+    }
+
+    private function isField($text)
+    {
+        return strpos($text, ':');
+    }
+
+    private function offsetGet($offset)
+    {
+        return function ($matches) use ($offset) {
+            return strtoupper($matches[$offset]);
+        };
+    }
+
+    private function opt2Curl(array $options)
+    {
+        $curl = curl_init();
+
+        $success = curl_setopt_array($curl, $options + $this->defaults);
+        assert('$success');
+
+        return $curl;
+    }
+
+    private function setField($header, $field)
+    {
+        list($key, $value) = preg_split('/:\s*/', $field, 2);
+        $key = strtolower($key);
+        $key = preg_replace_callback('/-(.)/', $this->offsetGet(1), $key);
+
+        $header[$key] = $value;
+
+        return $header;
+    }
+}
